29542d39453156049a1defbdd6105f675b9d48ad
dpiron <den.piron@gmail.com>
2023-04-30 09:56:21 +0200

    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,36 @@ from jinja2 import Environment, PackageLoader, select_autoescape, FileSystemLoad
 from datetime import date
 import streamlit as st
 from streamlit.components.v1 import iframe
+from google.oauth2 import service_account
+from gsheetsdb import connect
+
+# Create a connection object.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"],
+    scopes=[
+        "https://www.googleapis.com/auth/spreadsheets",
+    ],
+)
+conn = connect(credentials=credentials)
+
+
+# Perform SQL query on the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    rows = conn.execute(query, headers=1)
+    rows = rows.fetchall()
+    return rows
+
+
+sheet_url = st.secrets["private_gsheets_url"]
+rows = run_query(f'SELECT * FROM "{sheet_url}"')
+
+
+# Print results.
+for row in rows:
+    st.write(f"row")
+
 
 st.set_page_config(layout="centered", page_icon="🎓", page_title="Diploma Generator")
 st.title("🎓 Diploma PDF Generator")
